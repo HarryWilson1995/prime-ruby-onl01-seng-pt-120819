cfb0c47a30da214f85432b5c3ade461be8cdd3a5
Harry Wilson <harry.wilson1995a@gmail.com>
2019-12-12 23:24:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,11 +4,10 @@ def prime?(integer)
   divisible_range.any? do |divider|
     if integer % divider == 0 
       return false 
-    elsif integer == 2 
+      elsif integer == 2 
       return true 
-    else 
-      return true 
-    end 
+   end 
   end 
+  true 
 end 
  
