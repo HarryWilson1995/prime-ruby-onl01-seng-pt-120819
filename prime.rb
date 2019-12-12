1312d253fdc79ddabd8108d5b2306b2ad0206778
Harry Wilson <harry.wilson1995a@gmail.com>
2019-12-12 23:09:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,9 @@
 def prime(integer)
-  divisible_range 
+  divisible_range = (2..integer -1).to_a 
+  
+  divisible_range.any? do |divider|
+    if integer % divider == 0 
+      return false 
+    end 
+  end 
 end 
